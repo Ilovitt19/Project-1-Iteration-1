db290b08193973e79881161a5207196540a50bff
Ian <Ilovitt19@hotmail.com>
2015-02-28 13:32:52 -0600

    UI

--- src/Producer.java
+++ src/Producer.java
@@ -1,8 +1,23 @@
+import java.io.Serializable;
+
 /**
  * Created by Ian on 2/28/2015.
  */
-public class Producer {
+public class Producer implements Serializable {
 
 	private String name;
+	private String id;
+	private String address;
+	private String phone;
+	private double balance = 0;
+	private static final String CUSTOMER_STRING = "C";
 
+	public Producer(String name, String address, String phone){
+
+		this.name = name;
+		this.address = address;
+		this.phone = phone;
+		id = CUSTOMER_STRING;
+
+	}
 }
