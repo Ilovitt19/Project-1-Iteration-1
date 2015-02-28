0d83fe391944be0db34359774d8bdd349393afd5
Cassidy <casskayd@gmail.com>
2015-02-28 14:58:46 -0600

    getters and setters

--- src/Producer.java
+++ src/Producer.java
@@ -18,6 +18,49 @@ public class Producer implements Serializable {
 		this.address = address;
 		this.phone = phone;
 		id = CUSTOMER_STRING;
-
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public void generateID(){
+		
+	}
+
 }
